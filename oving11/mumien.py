a0afffb9da5cab4308d3f2ac662e89b203f9eeb7
simenhu <simen@primecut.no>
2016-11-15 13:40:46 +0100

    almost done

--- oving11/mumien.py
+++ oving11/mumien.py
@@ -1,38 +1,41 @@
 from sys import stdin, stderr
 
+nm = [[0, 1, 1, 0, 0, 0], [1, 0, 0, 1, 1, 0], [1, 0, 0, 1, 1, 0], [0, 1, 1, 0, 0, 1], [0, 1, 1, 0, 0, 1], [0, 0, 0, 1, 1, 0]]
+prob = [1.0, 0.9, 0.3, 0.1, 0.8, 1.0]
+
 def best_path(nm, prob):
     current_node = 0
     updated_weights = [x for x in range(len(nm))] #list to hold updated max_weights
-    updated_weights[0] = nm[0][0] #sets the max weight of the start node to the start node
-    predecesor_list = [-1 for x in range(len(nm))]
+    updated_weights[0] = prob[0] #sets the max weight of the start node to the start node
+    predecesor_list = [None for x in range(len(nm))]
     visited_count = 0
-
-    while nm[len(nm)-1][current_node] != 1 and visited_count < len(nm)-1:
+    timer = 0
+    while predecesor_list[len(nm)-1] == None:
         #iterates until all nodes have been visited or the max node is neighbour to finnish node
-        for n in range(len(nm)):
-            old_value = updated_weights[n]
-            new_value = updated_weights[current_node]*prob[n]
-            if new_value > old_value:
-                updated_weights[n] =
+        for n in range(len(nm)): #iterates through new visiteds neigbours
+            if nm[current_node][n] == 1 and updated_weights[n] > -1:
+                old_value = updated_weights[n] #the old max value for node n
+                new_value = updated_weights[current_node]*prob[n] #the new max value for node n
+                if new_value > old_value:
+                    #if the new value is  bigger than the old value and isnot already visited
+                    updated_weights[n] = new_value #updates the weight of the nodes
+                    predecesor_list[n] = current_node #
 
-        updated_weights[current_node] = -1 #this marks every node that has been visited with a negative number
+        updated_weights[current_node] = -1 #marks that the node is visited with negative value
+        current_node = updated_weights.index(max(updated_weights))
+        timer
         visited_count += 1
+        timer+=1
+        print(timer)
+    print(predecesor_list)
 
 
 
 
 
 
-n = int(stdin.readline())
-probabilities = [float(x) for x in stdin.readline().split()]
-neighbour_matrix = []
-for line in stdin:
-    neighbour_row = [0] * n
-    neighbours = [int(neighbour) for neighbour in line.split()]
-    for neighbour in neighbours:
-        neighbour_row[neighbour] = 1
-    neighbour_matrix.append(neighbour_row)
-print(neighbour_matrix)
-print(probabilities)
+
+
 #print (best_path(neighbour_matrix, probabilities))
 
+best_path(nm, prob)
